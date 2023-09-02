8ca13e4b577ba664cc9069bb7d61687937b04898
CAMIRO <juancamilosc@hotmail.com>
2023-09-02 09:27:45 -0500

    Suspense

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,10 +1,11 @@
 import PostCard from '../../components/PostCard'
 
+
 async function loadPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await res.json()
 
-    // await new Promise(res => setTimeout( res, 3000)) 
+    await new Promise(res => setTimeout( res, 5000)) 
     return data
 }
 
